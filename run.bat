93748b773de5269109327a4d95ccbcc462d5adcf
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-06-30 11:15:57 -0500

    build script

---
+++ run.bat
@@ -0,0 +1,5 @@
+@echo off
+ren SetBuild.SCRIPT SetBuild.hx
+haxe -m SetBuild --interp %~1 %~2 %~3 %~4 %~5 %~6 %~7 %~8 %~9
+ren SetBuild.hx SetBuild.SCRIPT
+cd ../.. && cls && funkin && cd mods/fightFunk
