d046ce7c795b42461a898eecd900d40ed65f4eb6
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-06-30 11:34:54 -0500

    healthbar stuffs

--- scripts/maki/fightfunk/FightUI.hx
+++ scripts/maki/fightfunk/FightUI.hx
@@ -3,6 +3,7 @@ package funkin.maki.fightfunk;
 import funkin.modding.module.Module;
 import funkin.play.PlayState;
 import funkin.mobile.input.ControlsHandler;
+import flixel.math.FlxMath;
 
 class FightUI extends Module
 {
@@ -48,7 +49,22 @@ class FightUI extends Module
 	{
 		fightUIEnabled = true;
 
+		game.remove(game.scoreText);
 		game.remove(game.comboPopUps);
+
+		var healthBarY = game.healthBarBG.y;
+
+		for (HBP in [game.healthBarBG, game.healthBar,])
+		{
+			HBP.scale.set(1.5, 1);
+			HBP.updateHitbox();
+			HBP.screenCenter();
+		}
+
+		game.healthBarBG.y = healthBarY;
+
+		game.healthBar.x = game.healthBarBG.x + 4;
+		game.healthBar.y = game.healthBarBG.y + 4;
 
 		game.refresh();
 	}
@@ -97,6 +113,11 @@ class FightUI extends Module
 	{
 		if (!game.isInCutscene)
 		{
+			// if (game.isBotPlayMode)
+			// 	game.healthLerp = Constants.HEALTH_MAX;
+			// else
+			// 	game.healthLerp = FlxMath.lerp(game.healthLerp, game.health, 0.000000015);
+
 			game.currentCameraZoom = 0.5;
 			game.defaultHUDCameraZoom = 1;
 			game.hudCameraZoomIntensity = 0;
